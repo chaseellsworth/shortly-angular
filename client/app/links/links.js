d843885bcef2a2c0bbbac4fc5721e0dbf1d1f5d8
chaseme3 <chaseme3@gmail.com>
2014-11-24 18:29:14 -0800

    Pass all tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,21 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $http) {
   // Your code here
+
+  $scope.data = {};
+
+  $scope.getLinks = function(){
+
+    $http.get('/api/links').
+      success(function(data, status, headers, config) {
+        $scope.data.links = data;
+      }).error(function(data, status, headers, config) {
+
+    });
+  };
+
+  $scope.getLinks();
+
 });
+
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,19 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $http) {
   // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function(){
+    $http.post('/api/links', JSON.stringify($scope.link)).
+      success(function(data, status, headers, config) {
+        $scope.link.url = '';
+      }).error(function(data, status, headers, config) {
+
+    });
+  };
+
 });
+
+
+
